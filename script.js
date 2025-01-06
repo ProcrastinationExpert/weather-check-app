912ba024ece05028bf68c84f4ccfc3ba51282cd4
ProcrastionationExpert <4zzyhy@gmail.com>
2025-01-06 22:56:34 +0700

    Created my first web app : Weather Check App

---
+++ script.js
@@ -0,0 +1,51 @@
+const API_KEY = "3e7e81d175865be50ff70dd60a3dd94a";
+const API_URL = "https://api.openweathermap.org/data/2.5/weather?&units=metric";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+  const response = await fetch(API_URL + "&q=" + city + `&appid=` + API_KEY);
+
+  if (response.status === 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    var data = await response.json();
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "Images/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "Images/clear.png";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "Images/rain.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "Images/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.src = "Images/mist.png";
+    } else if (data.weather[0].main == "Snow") {
+      weatherIcon.src = "Images/snow.png";
+    }
+
+    document.querySelector(".error").style.display = "none";
+    document.querySelector(".weather").style.display = "block";
+  }
+}
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
+
+searchBox.onkeydown = function (e) {
+  if (e.keyCode == 13 && searchBox.value != "") {
+    checkWeather(searchBox.value);
+  }
+};
